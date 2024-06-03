a56a72fc792f7a1ac199ba160bf00534ded1fc26
Krypton <shakirgulzar0x00@gmail.com>
2024-06-03 05:26:35 -0700

    messages & Conversation Models added

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import authRoute from "./routes/auth.routes.js";
+import messageRoute from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+
 dotenv.config();
+app.use(cors());
+
+
 
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRoute);
+app.use("/api/messages", messageRoute);
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
